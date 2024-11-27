adfdad2c6f89c78f9be2f52b7e011b6ef58dce4e
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2024-11-27 08:36:02 +0800

    Ability to switch between pages

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ import about from "./pages/about";
 import "./styles.css";
 
 const content = document.querySelector("#content");
+const btnLinks = document.querySelectorAll(".btn-link");
 
 function loadPage() {
   addEventListener("load", () => {
     content.innerHTML = home();
+
+    btnLinks.forEach((btnLink) => btnLink.addEventListener("click", () => {
+      if (btnLink.textContent === "Home") {
+        content.innerHTML = home();
+      } else if (btnLink.textContent === "Menu") {
+        content.innerHTML = menu();
+      } else if (btnLink.textContent === "About") {
+        content.innerHTML = about();
+      } else {
+        content.innerHTML = home();
+      }
+    }));
   })
 }
 
