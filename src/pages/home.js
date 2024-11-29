4bacca9a6e7a85047c208e9f43488ff1376afc03
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2024-11-29 08:03:14 +0800

    Change page title, modify text in home

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ const home = () => {
   return (`
   <div class="row">
     <div class="content-text">
-      <h2>A Journey Through Asian Delights</h2>
+      <h2>A Journey Through Indonesia Delights</h2>
       <p>Bringing the world’s most vibrant flavors to your table.</p>
 
       <div class="btn-group">
